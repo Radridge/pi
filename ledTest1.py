568a36c3d58db1981fd1e3043874a6cc8cb2797f
Benny Stone <stone@radridge.com>
2022-06-02 19:07:11 -0700

    git commit

--- ledTest1.py
+++ ledTest1.py
@@ -4,19 +4,27 @@ import time
 LED_LIST = [17, 27, 22]
 BUTTON = 26
 
+def singleLightOn(lightNumber):
+    for pin in LED_LIST:
+        if pin == lightNumber:
+            GPIO.output(pin, GPIO.HIGH)
+        else:
+            GPIO.output(pin, GPIO.LOW)
+
 GPIO.setmode(GPIO.BCM)
 for lit in LED_LIST:
     GPIO.setup(lit, GPIO.OUT)
 GPIO.setup(BUTTON, GPIO.IN)
 
+prevBVal = GPIO.input(BUTTON)
+ledIndex = 0
 while True:
-    bVal = GPIO.input(BUTTON)
     time.sleep(0.1)
-    if bVal == 1:
-        for lits in LED_LIST:
-            GPIO.output(lits, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(lits, GPIO.LOW)
-            time.sleep(0.5)
-
-GPIO.cleanup()
+    bVal = GPIO.input(BUTTON)
+    if bVal != prevBVal:
+        prevBVal = bVal
+        if bVal == GPIO.HIGH:
+            singleLightOn(LED_LIST[ledIndex])
+            ledIndex += 1
+            if ledIndex >= len(LED_LIST):
+                ledIndex = 0 
